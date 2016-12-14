e0277834abb6f01f687d0e67e2a940e816f45af7
Michael Scherer <michael.c.scherer@gmail.com>
2016-12-14 10:15:05 -0700

    Refactor faraday connection to initialize method

--- app/services/stations_service.rb
+++ app/services/stations_service.rb
@@ -1,12 +1,14 @@
 class StationsService
 
-  def find_stations(zipcode, radius)
-    conn = Faraday.new(:url => 'https://developer.nrel.gov') do |faraday|
+  def initialize
+    @conn = Faraday.new(:url => 'https://developer.nrel.gov') do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
       faraday.response :logger                  # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
+  end
 
+  def find_stations(zipcode, radius)
     response = conn.get '/api/alt-fuel-stations/v1/nearest.json', {
                               api_key: ENV["nrel_api_key"],
                               location: zipcode,
@@ -15,5 +17,8 @@ class StationsService
 
     parsed_response = JSON.parse(response.body, symbolize_names: true)
   end
+
+  private
+    attr_reader :conn
 
 end
